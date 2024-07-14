731c86610af0a47cd80d3c0d96cdcb3468c27342
jorgemeyrelles <jotaengpuc@gmail.com>
2024-07-14 17:18:57 -0300

    feat: database

--- src/config/dataSource.ts
+++ src/config/dataSource.ts
@@ -8,7 +8,7 @@ export const AppDataSource = new DataSource({
   host: "localhost",
   port: 3306,
   username: "root",
-  password: "Jl!24453687",
+  password: "*********",
   database: "bd_alura_typescript",
   synchronize: true,
   logging: true,
